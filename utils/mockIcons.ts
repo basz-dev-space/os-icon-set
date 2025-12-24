aa279422c961a92e3e76cbdae654f43b0ba12feb
Basz Lightbump <bosz-dev-space@outlook.com>
2025-12-25 01:49:18 +0700

    Add mock icons data

---
+++ utils/mockIcons.ts
@@ -0,0 +1,29 @@
+export const mockIcons = [
+  {
+    id: 1,
+    name: "Windows 10 Logo",
+    os: "windows",
+    version: "10",
+    format: "svg",
+    path: "/icons/windows/10/logo.svg",
+    isPublic: true,
+  },
+  {
+    id: 2,
+    name: "Windows 11 Logo",
+    os: "windows",
+    version: "11",
+    format: "svg",
+    path: "/icons/windows/11/logo.svg",
+    isPublic: true,
+  },
+  {
+    id: 3,
+    name: "macOS Ventura Logo",
+    os: "macos",
+    version: "ventura",
+    format: "svg",
+    path: "/icons/macos/ventura/logo.svg",
+    isPublic: true,
+  },
+];
